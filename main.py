842c0cd34327c3b4753083e3bd8682455f639e3b
James Maslek <jmaslek11@gmail.com>
2023-08-17 09:57:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ start_time = time.time()
 import pandas 
 
 end_time = time.time()
-print("Script execution took %d seconds" % (end_time - start_time))
+print("Script execution took %.4f seconds" % round(elapsed, 4))
